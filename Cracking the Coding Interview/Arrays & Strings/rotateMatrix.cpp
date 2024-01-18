76a5b97c797715862ce5dcaf32d100320fe82be2
Ashish Lathkar <63287421+Ashishlathkar77@users.noreply.github.com>
2024-01-18 13:04:34 +0530

    rotateMatrix.cpp

--- Cracking the Coding Interview/Arrays & Strings/rotateMatrix.cpp
+++ Cracking the Coding Interview/Arrays & Strings/rotateMatrix.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <algorithm>  // Include algorithm for reverse function
+#include <algorithm>  
 using namespace std;
 
 void rotateMatrix(int matrix[][100], int n, int m)
 {
     for (int i = 0; i < n; i++)
     {
-        for (int j = i + 1; j < m; j++) // Fix the loop limit to m
+        for (int j = i + 1; j < m; j++) 
         {
             swap(matrix[i][j], matrix[j][i]);
         }
@@ -14,12 +14,12 @@ void rotateMatrix(int matrix[][100], int n, int m)
 
     for (int i = 0; i < n; i++)
     {
-        reverse(matrix[i], matrix[i] + m); // Use reverse on the entire row
+        reverse(matrix[i], matrix[i] + m); 
     }
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++) // Fix the loop limit to m
+        for (int j = 0; j < m; j++) 
         {
             cout << matrix[i][j] << ' ';
         }
@@ -31,7 +31,7 @@ int main()
 {
     int n, m;
     cin >> n >> m;
-    int matrix[100][100]; // Adjust the size accordingly
+    int matrix[100][100]; 
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
